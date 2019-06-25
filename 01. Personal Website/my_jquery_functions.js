473eaa0471f4ef6e62c04550e32718bce239da11
FatimaCoulibaly <42788249+FatimaCoulibaly@users.noreply.github.com>
2019-06-25 15:57:09 -0400

    Add files via upload

---
+++ 01. Personal Website/my_jquery_functions.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+var instance = M.Parallax.getInstance(elem);
+
+  /* jQuery Method Calls
+    You can still use the old jQuery plugin method calls.
+    But you won't be able to access instance properties.
+
+    $('.parallax').parallax('methodName');
+    $('.parallax').parallax('methodName', paramName);
+  */
+        
